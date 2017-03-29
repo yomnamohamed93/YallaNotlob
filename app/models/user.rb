79f9aabe12467e6b816dea296d1d05b51359ccee
m-foda <m.foda91@gmail.com>
2017-03-30 01:43:42 +0200

    some linting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # Just to silence the linter
 class User < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
+  has_attached_file :avatar,
+                    styles: { medium: '300x300>', thumb: '100x100>' },
+                    default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
 end
