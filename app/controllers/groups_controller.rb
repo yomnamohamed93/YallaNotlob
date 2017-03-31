6e7ea0da84838a025ccba0c5f658fde33f0b17ff
m-foda <m.foda91@gmail.com>
2017-03-31 14:50:03 +0200

    edited

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
   # GET /groups
   # GET /groups.json
   def index
-    @user = User.find(params[:user_id])
+    @user = User.find(current_user)
     @groups = @user.groups
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  # resources :users
   root 'users#index'
-  resources :users do
-    resources :groups
-  end
+  resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
