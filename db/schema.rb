83695f44188d4d66573999e7ac3738bde1902a7a
m-foda <m.foda91@gmail.com>
2017-04-15 16:17:30 +0200

    public folder

--- db/schema.rb
+++ db/schema.rb
@@ -101,8 +101,6 @@ ActiveRecord::Schema.define(version: 20170414151354) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "provider"
-    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
